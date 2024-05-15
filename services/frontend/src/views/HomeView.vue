f168ded67f44c23bd68f419a2cbce433c7e5d936
Anis Koubaa <mohamed.koubaa@kit.edu>
2024-05-15 13:44:53 +0200

    add utilities folder

--- services/frontend/src/views/HomeView.vue
+++ services/frontend/src/views/HomeView.vue
@@ -12,7 +12,7 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/app/shared/components/HelloWorld.vue'
-import ButtonCounter from '@/app/shared/components/ButtonCounter.vue'
+import ButtonCounter from '@/app/shared/components/app-utilities/ButtonCounter.vue'
 import Navigation from '@/app/shared/components/app-header/NavigationBar.vue'
 import FooterBar from "@/app/shared/components/app-footer/FooterBar.vue";
 
